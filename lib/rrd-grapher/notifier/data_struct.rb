053ec932a4d39656f52f73920d657ec7c2d0c686
Julien Ammous <j.ammous@gmail.com>
2011-08-22 10:56:39 +0200

    DataStruct: Allow using a hash with string keys

--- lib/rrd-grapher/notifier/data_struct.rb
+++ lib/rrd-grapher/notifier/data_struct.rb
@@ -16,9 +16,8 @@ class DataStruct
   # 
   def merge_data_from(opts_or_obj = {}, only_fields = nil, allow_nil = false)
     self.class.attributes.select{|attr_name| selected_field?(attr_name, only_fields) }.each do |attr_name|
-      v = opts_or_obj.is_a?(Hash) ? opts_or_obj[attr_name] : opts_or_obj.send(attr_name)
+      v = opts_or_obj.is_a?(Hash) ? (opts_or_obj[attr_name.to_s] || opts_or_obj[attr_name]) : opts_or_obj.send(attr_name)
       if allow_nil || !v.nil?
-        # puts "#{attr_name} => #{v}"
         send("#{attr_name}=", v)
       end
     end
